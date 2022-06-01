6491733a002873d1447ea8e5d4924adeff9b0237
Nico Zampolini <zampolini7@gmail.com>
2022-06-01 16:06:18 -0300

    First changes after deploy

--- src/App.jsx
+++ src/App.jsx
@@ -204,7 +204,7 @@ const App = () => {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to coffeDAO</h1>
         <button onClick={connectWithMetamask} className="btn-hero">
           Connect your wallet
         </button>
